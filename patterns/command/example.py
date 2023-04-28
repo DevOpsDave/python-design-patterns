ad97260ba254dd2d6bf78da6fac0433c59dfff04
Dave Barcelo <jdbarcelo@gmail.com>
2023-04-28 08:44:34 -0400

    finished course

--- patterns/command/example.py
+++ patterns/command/example.py
@@ -3,6 +3,8 @@
 """
 Why would someone use the command pattern?  
 You can use it to run some action and by keeping the state of the action in the command you can undo it.
+This is good because we can then pass the command around as an object.  We can put it in a stack or something
+and keep a command histroy.  
 """
 
 import os
